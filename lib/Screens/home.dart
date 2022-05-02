e0f84e9d3a4971aa634cbc80f6962a68df442bf9
Ali Punjabi <raja.ehtisham222@gmail.com>
2022-05-03 02:25:55 +0500

    Project Completed

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -9,8 +9,13 @@ import 'package:project1/Screens/screen5.dart';
 import 'package:project1/Screens/screen6.dart';
 import 'package:project1/Screens/screen9.dart';
 import 'package:project1/constants.dart';
+import 'package:intl/intl.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class home extends StatelessWidget {
+  static final DateTime now = DateTime.now();
+  static final DateFormat formatter = DateFormat('dd-MM-yyyy');
+  final String formatted = formatter.format(now);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,29 +40,95 @@ class home extends StatelessWidget {
           ]),
       body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                formatted,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "\"A friend in need is a friend indeed\"",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8), // if you need this
+                    side: BorderSide(
+                      color: Colors.grey.withOpacity(0.2),
+                      width: 1,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "An apple a day keeps the doctor away",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularPercentIndicator(
+                    radius: 45.0,
+                    lineWidth: 4.0,
+                    percent: 0.10,
+                    center: Text("10%"),
+                    progressColor: Colors.red,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  ),
+                  CircularPercentIndicator(
+                    radius: 45.0,
+                    lineWidth: 4.0,
+                    percent: 0.30,
+                    center: Text("30%"),
+                    progressColor: Colors.orange,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  ),
+                  CircularPercentIndicator(
+                    radius: 45.0,
+                    lineWidth: 4.0,
+                    percent: 0.60,
+                    center: Text("60%"),
+                    progressColor: Colors.yellow,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  ),
+                ],
+              ),
+            ),
             SizedBox(width: double.infinity),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: CustomElevatedButton(
-                    // cutom button for signup, you can check it properties from lib/Customs/button.dart
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        Transitions(
-                            transitionType: TransitionType.fade,
-                            curve: Curves.bounceInOut,
-                            duration: const Duration(milliseconds: 500),
-                            reverseCurve: Curves.bounceOut,
-                            widget:
-                                screen5()), // on tap, it will leads you to pillscreen
-                      );
-                    },
-                    child: Text('Screen 5', style: TextStyle())),
-              ),
-            ),
             SizedBox(
               height: 15,
             ),
@@ -66,75 +137,12 @@ class home extends StatelessWidget {
               child: SizedBox(
                 width: double.infinity,
                 child: CustomElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        Transitions(
-                            transitionType: TransitionType.fade,
-                            curve: Curves.bounceInOut,
-                            duration: const Duration(milliseconds: 500),
-                            reverseCurve: Curves.bounceOut,
-                            widget:
-                                screen6()), // on tap, it will leads you to pillscreen
-                      );
-                    },
-                    child: Text('Screen 6',
+                    onPressed: () {},
+                    child: Text('Trending Nutrition Events/Sports in UAE',
                         style: TextStyle(
                             // check lib/Customs/Constants.dart for all colors and fonts used in this app
                             ))),
               ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: CustomElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        Transitions(
-                            transitionType: TransitionType.fade,
-                            curve: Curves.bounceInOut,
-                            duration: const Duration(milliseconds: 500),
-                            reverseCurve: Curves.bounceOut,
-                            widget:
-                                screen9()), // on tap, it will leads you to pillscreen
-                      );
-                    },
-                    child: Text('Screen 9',
-                        style: TextStyle(
-                            // check lib/Customs/Constants.dart for all colors and fonts used in this app
-                            ))),
-              ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: double.infinity,
-                child: CustomElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        Transitions(
-                            transitionType: TransitionType.fade,
-                            curve: Curves.bounceInOut,
-                            duration: const Duration(milliseconds: 500),
-                            reverseCurve: Curves.bounceOut,
-                            widget:
-                                screen10()), // on tap, it will leads you to pillscreen
-                      );
-                    },
-                    child: Text('Screen 10',
-                        style: TextStyle(
-                            // check lib/Customs/Constants.dart for all colors and fonts used in this app
-                            ))),
-              ),
-            ),
-            SizedBox(
-              height: 15,
             ),
           ]),
     );
